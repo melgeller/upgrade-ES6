07ade5e8b2e27077348d56a199fa73d6457f033f
melgeller <melgeller.foto@gmail.com>
2022-05-23 17:12:26 +0200

    iteracion1

---
+++ iteracion1.js
@@ -0,0 +1,13 @@
+
+//1.1 Ejecuta esta función sin pasar ningún parametro
+
+const sumar = (a = 10, b = 5) => a + b;
+console.log(sumar());
+
+//1.2 Ejecuta esta función pasando un solo parametro
+
+console.log(sumar(2));
+
+//1.3 Ejecuta esta función pasando dos parametros
+
+console.log(sumar(2,2));
